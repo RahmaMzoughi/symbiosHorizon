8b2e22cfefc457965cd788f7cf0c254d3e8961ba
Rahma MZOUGHI <rahma.mzoughi@sofrecom.com>
2022-06-17 09:01:02 +0100

    Initial commit ..

---
+++ src/main/java/com/horizon/symbios/SymbiosApplication.java
@@ -0,0 +1,14 @@
+package com.horizon.symbios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbiosApplication.class, args);
+		System.out.println("hellooooooooooo");
+	}
+
+}
---
+++ src/test/java/com/horizon/symbios/SymbiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizon.symbios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
